53204373a3eb52faa8a9438b6552b7f7190a170c
fernansd <i42sadef@uco.es>
2016-05-04 13:59:34 +0200

    Falta implementar los métodos de la clase

--- p4/grafo.cpp
+++ p4/grafo.cpp
@@ -1,1 +1,24 @@
 #include "grafo.hpp"
+namespace ed {
+
+/// Observers
+int adyacente();
+bool cursorEnVertice();
+Vertice<string> verticeCursor();
+bool cursorEnLado();
+int ladoCursor();
+
+/// Mutators
+void addVertice();
+void addLado();
+bool buscarVertice();
+bool goTo();
+bool buscarLado();
+Vertice<string> verticeInicio();
+Vertice<string> verticeSiguiente();
+bool afterEndVertex();
+int ladoInicio(Vertice<string> const& v);
+int ladoSiguiente();
+bool afterEndEdge();
+
+}
--- p4/grafo.hpp
+++ p4/grafo.hpp
@@ -1,9 +1,50 @@
+#include <vector>
+#include <string>
+#include <limits> // Para el valor numérico infinito
+
+#include "vertice.hpp"
+
 // Poner documentación de fichero
 namespace ed {
 
+using std::vector;
+using std::string;
+
 // Documentar clase
 class Grafo {
+	
+	private:
+		bool es_dirigido;
+		/// Apunta al vertice actual, mutable para permitir grafos constantes
+		mutable int cursor_;
+		vector<Vertice<string>> vertices_;
+		vector<vector<double>> matriz_;
 	public:
+		Grafo() {
+			double inf = std::numeric_limits<double>::infinity();
+			// Por defecto el grafo es no dirigido
+			es_dirigido = false;
+		}
+	
+		/// Creators
+		void hacerDirigido() { es_dirigido = true; }
+		void hacerNoDirigido() { es_dirigido = false; }
 		
+		/// Observers
+		int numVertices() const { return vertices_.size(); }
+		int numLados() const { return matriz_.size(); }
+		bool esDirigido() { return es_dirigido; }
+		bool estaVacio() { return vertices_.empty(); }
+		int adyacente() const;
+		Vertice<string> verticeCursor() const;
+		
+		/// Mutators
+		void addVertice();
+		void addLado();
+		bool buscarVertice() const;
+		bool goTo() const;
+		Vertice<string> verticeInicio();
+		Vertice<string> verticeSiguiente();
+		bool afterEndVertex();
 };
 } // Fin namespace ed
